49e4a90fb80a0856d276b02037449980d26495dc
Gabriel Vieira Vidal de Carvalho <gabrielvieiravidal2005@gmail.com>
2023-11-30 12:11:34 -0300

    Bottom bar completa, com cores e ícones adicionados.

--- src/routes/tab.routes.js
+++ src/routes/tab.routes.js
@@ -4,15 +4,68 @@ import Pedidos from "../screens/Pedidos"
 import Favoritos from "../screens/Favoritos"
 import Perfil from "../screens/Perfil"
 
+import { Feather } from '@expo/vector-icons';
+
 const Tab = createBottomTabNavigator();
 
 export default function TabRoutes() {
   return(
-    <Tab.Navigator screenOptions={{ headerShown: false}}>
-      <Tab.Screen name="Início" component={Inicio}/>
-      <Tab.Screen name="Pedidos" component={Pedidos}/>
-      <Tab.Screen name="Favoritos" component={Favoritos}/>
-      <Tab.Screen name="Perfil" component={Perfil}/>
+    <Tab.Navigator screenOptions={{ 
+      headerShown: false,
+      tabBarStyle: {
+        backgroundColor: '#ffffff',
+        borderTopWidth: 0,
+        shadowColor:'transparent',
+        paddingBottom: 6.5,
+        paddingTop: 6.5,
+        height: 70,
+        columnGap: 35
+      },
+      tabBarLabelStyle: {
+        fontSize: 12,
+      },
+      tabBarActiveTintColor: '#009bff',
+      tabBarInactiveTintColor: '#000000'
+    }}>
+      <Tab.Screen 
+        name="Início" 
+        component={Inicio}
+        options={{
+          tabBarIcon: ({focused}) => {
+            return <Feather name="home" size={24} color={focused ? "#009bff" : "#000000"} />
+          }
+        }}
+      />
+
+      <Tab.Screen 
+        name="Pedidos" 
+        component={Pedidos}
+        options={{
+          tabBarIcon: ({focused}) => {
+            return <Feather name="shopping-bag" size={24} color={focused ? "#009bff" : "#000000"} />
+          }
+        }}
+      />
+
+      <Tab.Screen 
+        name="Favoritos" 
+        component={Favoritos}
+        options={{
+          tabBarIcon: ({focused}) => {
+            return <Feather name="heart" size={24} color={focused ? "#009bff" : "#000000"} />
+          }
+        }}
+      />
+
+      <Tab.Screen 
+        name="Perfil" 
+        component={Perfil}
+        options={{
+          tabBarIcon: ({focused}) => {
+            return <Feather name="user" size={24} color={focused ? "#009bff" : "#000000"} />
+          }
+        }}
+      />
     </Tab.Navigator>
   )
 }
